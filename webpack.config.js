1c7ad492d7d18bfbf697d624d54cc8e99081d955
Berkay Altay <altayberkay1@gmail.com>
2021-09-17 17:06:55 +0300

    custom-build-classic

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,91 @@
-// webpack.config.js
+/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
 
-"use strict";
+'use strict';
 
-const path = require("path");
-const { styles } = require("@ckeditor/ckeditor5-dev-utils");
+/* eslint-env node */
+
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
+const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
+const TerserPlugin = require( 'terser-webpack-plugin' );
 
 module.exports = {
-  // https://webpack.js.org/configuration/entry-context/
-  entry: "./app.js",
+	devtool: 'source-map',
+	performance: { hints: false },
 
-  // https://webpack.js.org/configuration/output/
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
+	entry: path.resolve( __dirname, 'src', 'ckeditor.js' ),
 
-  module: {
-    rules: [
-      {
-        test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
-        use: ["raw-loader"],
-      },
-      {
-        test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
+	output: {
+		// The name under which the editor will be exported.
+		library: 'ClassicEditor',
 
-        use: [
-          {
-            loader: "style-loader",
-            options: {
-              injectType: "singletonStyleTag",
-              attributes: {
-                "data-cke": true,
-              },
-            },
-          },
-          {
-            loader: "postcss-loader",
-            options: styles.getPostCssConfig({
-              themeImporter: {
-                themePath: require.resolve("@ckeditor/ckeditor5-theme-lark"),
-              },
-              minify: true,
-            }),
-          },
-        ],
-      },
-    ],
-  },
+		path: path.resolve( __dirname, 'build' ),
+		filename: 'ckeditor.js',
+		libraryTarget: 'umd',
+		libraryExport: 'default'
+	},
 
-  // Useful for debugging.
-  devtool: "source-map",
+	optimization: {
+		minimizer: [
+			new TerserPlugin( {
+				sourceMap: true,
+				terserOptions: {
+					output: {
+						// Preserve CKEditor 5 license comments.
+						comments: /^!/
+					}
+				},
+				extractComments: false
+			} )
+		]
+	},
 
-  // By default webpack logs warnings if the bundle is bigger than 200kb.
-  performance: { hints: false },
+	plugins: [
+		new CKEditorWebpackPlugin( {
+			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
+			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
+			language: 'en',
+			additionalLanguages: 'all'
+		} ),
+		new webpack.BannerPlugin( {
+			banner: bundler.getLicenseBanner(),
+			raw: true
+		} )
+	],
+
+	module: {
+		rules: [
+			{
+				test: /\.svg$/,
+				use: [ 'raw-loader' ]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'style-loader',
+						options: {
+							injectType: 'singletonStyleTag',
+							attributes: {
+								'data-cke': true
+							}
+						}
+					},
+					{
+						loader: 'postcss-loader',
+						options: styles.getPostCssConfig( {
+							themeImporter: {
+								themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
+							},
+							minify: true
+						} )
+					}
+				]
+			}
+		]
+	}
 };
