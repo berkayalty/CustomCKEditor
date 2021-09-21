95fd29b987326e454d5dc13bdda7200cac001f60
Berkay Altay <altayberkay1@gmail.com>
2021-09-21 13:48:47 +0300

    update

--- src/InsertAudio.js
+++ src/InsertAudio.js
@@ -2,17 +2,19 @@ import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
 import ButtonView from "@ckeditor/ckeditor5-ui/src/button/buttonview";
 import AudioIcon from "./volumeHigh.svg";
 import axios from "axios";
+
 class InsertAudio extends Plugin {
   init() {
+    const buttonViewAttr = {
+      label: "Ses Ekle",
+      icon: AudioIcon,
+      tooltip: true,
+      withText: false,
+    };
     const editor = this.editor;
     editor.ui.componentFactory.add("insertAudio", (locale) => {
-      console.log("env", window.globalApiPath);
       const buttonView = new ButtonView(locale);
-      buttonView.set({
-        label: "Ses Ekle",
-        icon: AudioIcon,
-        tooltip: true,
-      });
+      buttonView.set(buttonViewAttr);
 
       buttonView.on("execute", () => {
         var fileSelector = document.createElement("input");
@@ -20,11 +22,11 @@ class InsertAudio extends Plugin {
         fileSelector.setAttribute("accept", "audio/*");
         fileSelector.click();
         fileSelector.onchange = function (event) {
-          var audioId = Math.random().toString(16).slice(2);
+          editor.isReadOnly = true;
           var file = event.target.files[0];
           let formData = new FormData();
-          formData.append("baslik", "ckeditorSesWitId");
-          formData.append("aciklama", audioId);
+          formData.append("baslik", "ckeditorSes");
+          formData.append("aciklama", "ckses");
           formData.append("dosya", file);
           axios
             .post(`${window.globalApiPath}api/file`, formData, {
@@ -39,16 +41,22 @@ class InsertAudio extends Plugin {
                   .get(`${window.globalApiPath}api/file/presignedURL/${sha1}`)
                   .then((response) => {
                     var s3URL = response.data.data;
-                    const content = `<audio controls id=${audioId} src=${s3URL} preload="none"></audio><br/>`;
+                    const content = `<audio controls src=${s3URL} preload="none"/><br/>`;
                     const viewFragment = editor.data.processor.toView(content);
                     const modelFragment = editor.data.toModel(viewFragment);
                     editor.model.insertContent(modelFragment);
+                    editor.isReadOnly = false;
                   })
-                  .catch((err) => {});
+                  .catch((err) => {
+                    editor.isReadOnly = true;
+                  });
               } else {
+                editor.isReadOnly = true;
               }
             })
-            .catch((err) => {});
+            .catch((err) => {
+              editor.isReadOnly = true;
+            });
         };
       });
       return buttonView;
